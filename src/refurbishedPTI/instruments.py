e584af0a9c551c69c7f7c281b960638a0dd4ec16
Ramiro Harguiunteguy <ramirohar@gmail.com>
2026-05-13 17:41:32 -0300

    refactor(axi-spectrometer): remove channel configuration, use the default

--- src/refurbishedPTI/instruments.py
+++ src/refurbishedPTI/instruments.py
@@ -861,7 +861,7 @@ class AxiSpectrometer(abstract.Spectrometer):
                 **configs.EMISSION_MONO_DRIVER
             )
         if osc is None:
-            osc = rpp.AxiOscilloscope(constants.ChannelConfig.CH1_ONLY)
+            osc = rpp.AxiOscilloscope()
             return cls(excitation_mono, emission_mono, osc, home=home)
 
     # TODO: leave this method here or directly call self.emission_mono.goto_wavelength
